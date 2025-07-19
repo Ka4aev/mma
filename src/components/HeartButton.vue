cc42ce46d504540180a7e3bbcb3961939abd9348
Ka4aev <staskachaev205@mail.ru>
2025-07-19 20:34:49 +0700

    .

--- src/components/HeartButton.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div style="display: flex; justify-content: center; margin: 24px 0;">
-        <svg width="30" height="30" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"
-          style="cursor: pointer;" @click="celebrate">
-          <path d="M24 43s-1.45-1.32-3.2-2.91C12.2 33.13 4 26.36 4 18.5 4 12.15 9.15 7 15.5 7c3.54 0 6.73 1.73 8.5 4.44C25.77 8.73 28.96 7 32.5 7 38.85 7 44 12.15 44 18.5c0 7.86-8.2 14.63-16.8 21.59C25.45 41.68 24 43 24 43z" fill="#f472b6"/>
-        </svg>
-      </div>
-</template>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './style.css'
-import Particles from 'particles.vue3'
 
-const app = createApp(App)
-app.use(Particles)
-app.mount('#app')
+createApp(App).mount('#app')
